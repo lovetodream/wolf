9d94cc46496fada27865e2ba44fc522cf3ffbe2c
Timo <38291523+lovetodream@users.noreply.github.com>
2021-07-06 19:09:51 +0200

    ✨ Configure mongodb/mongoose

--- packages/api/src/app/app.module.ts
+++ packages/api/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { environment } from '../environments/environment';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(environment.mongodb)],
   controllers: [AppController],
   providers: [AppService],
 })
--- packages/api/src/environments/environment.prod.ts
+++ packages/api/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  mongodb: process.env.MONGODB
 };
