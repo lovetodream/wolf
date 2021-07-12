11a694975f2aaf087d4805c4472bc5dc363f14fd
Timo <38291523+lovetodream@users.noreply.github.com>
2021-07-12 19:23:49 +0200

    ✨ Add lazy loaded modal component

--- packages/frontend/src/app/app.module.ts
+++ packages/frontend/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { ModalModule } from './modal/modal.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([{ path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }], { initialNavigation: 'enabledBlocking' }),
+    GraphqlModule,
+    ModalModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ packages/frontend/src/app/modal/modal.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { ModalService } from './modal.service';
+
+@Component({
+  selector: 'wolf-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss'],
+})
+export class ModalComponent<T> {
+  display = true;
+
+  constructor(private modalService: ModalService<T>) {}
+
+  async close(): Promise<void> {
+    this.display = false;
+
+    setTimeout(async () => {
+      await this.modalService.close();
+    }, 300);
+  }
+}
