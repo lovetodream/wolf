7f61d2df9abcad627fb495c3286d3f4d94798fd9
Timo <38291523+lovetodream@users.noreply.github.com>
2021-08-08 19:55:38 +0200

    🔧 Move tailwind to just in time mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
 const { guessProductionMode } = require("@ngneat/tailwind");
 
 module.exports = {
-    prefix: '',
-    purge: {
-      enabled: guessProductionMode(),
-      content: [
-        './packages/**/*.{html,ts}',
-        './packages/**/*.{html,ts}',
-      ]
+  mode: 'jit',
+  prefix: '',
+  purge: {
+    enabled: guessProductionMode(),
+    content: [
+      './packages/**/*.{html,ts}',
+      './packages/**/*.{html,ts}',
+    ]
+  },
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      padding: ['hover', 'group-hover']
     },
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {
-        padding: ['hover', 'group-hover']
-      },
-    },
-    plugins: [require('@tailwindcss/aspect-ratio'),require('@tailwindcss/typography'),require('@tailwindcss/forms')],
+  },
+  plugins: [require('@tailwindcss/aspect-ratio'),require('@tailwindcss/typography'),require('@tailwindcss/forms')],
 };
